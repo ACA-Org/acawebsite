4ba4bb3d14d1fb7b9801e5834fc523d3cab12a98
zacmelendez <zacmelendez@gmail.com>
2025-05-28 10:37:02 -0400

    fix: assign link to card container

--- src/slices/TileContainer/index.tsx
+++ src/slices/TileContainer/index.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
-import { LinkButton } from "@/components/ui/button";
-import { PrismicNextImage } from "@prismicio/next";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import React from "react";
 
 /**
@@ -40,49 +41,34 @@ const TileContainer: FC<TileContainerProps> = ({ slice }) => {
       )}
       {tiles.length > 0 && (
         <div className="flex flex-wrap gap-6">
-          {tiles.map((tile, i) => (
-            <React.Fragment key={i}>
-              {tile.tileLink.text ? (
-                <div
+          {tiles.map(({ tileDesc, tileHeading, tileImage, tileLink }, i) => {
+            console.log({ tileLink });
+            const Comp =
+              tileLink?.link_type !== "Any" ? PrismicNextLink : "div";
+            return (
+              <React.Fragment key={i}>
+                <Comp
                   key={i}
                   className="relative flex min-h-[250px] max-w-1/3 flex-1 items-end overflow-clip rounded-md bg-none p-6"
+                  field={tileLink}
                 >
                   <div className="absolute bottom-0 left-0 z-1 h-full w-full bg-gradient-to-t from-[#0F2D52] to-transparent" />
                   <figure className="absolute top-0 left-0 -z-1 h-full w-full">
                     <PrismicNextImage
-                      field={tile.tileImage}
+                      field={tileImage}
                       alt=""
                       className="h-full w-full object-cover"
                     />
                   </figure>
                   <div className="relative z-2 flex flex-col gap-2 text-white">
-                    <span className="heading-4">{tile.tileHeading}</span>
-                    <span>{tile.tileDesc}</span>
-                    {tile.tileLink?.text && (
-                      <LinkButton field={tile.tileLink}>
-                        {tile.tileLink.text}
-                      </LinkButton>
-                    )}
+                    <span className="heading-4">{tileHeading}</span>
+                    <span>{tileDesc}</span>
+                    {tileLink?.text && <span>{tileLink?.text}</span>}
                   </div>
-                </div>
-              ) : (
-                <div className="relative flex min-h-[250px] max-w-1/3 flex-1 items-end overflow-clip rounded-md p-6">
-                  <div className="absolute bottom-0 left-0 z-1 h-full w-full bg-gradient-to-t from-[#0F2D52] to-transparent" />
-                  <figure className="absolute top-0 left-0 -z-1 h-full w-full">
-                    <PrismicNextImage
-                      field={tile.tileImage}
-                      alt=""
-                      className="h-full w-full object-cover"
-                    />
-                  </figure>
-                  <div className="relative z-2 flex flex-col gap-2 text-white">
-                    <span className="heading-4">{tile.tileHeading}</span>
-                    <span>{tile.tileDesc}</span>
-                  </div>
-                </div>
-              )}
-            </React.Fragment>
-          ))}
+                </Comp>
+              </React.Fragment>
+            );
+          })}
         </div>
       )}
     </section>
