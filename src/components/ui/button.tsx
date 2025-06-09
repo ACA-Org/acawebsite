8b1778fe73d8c82702ae7cb0baf8b7c44fefad92
zacmelendez <zacmelendez@gmail.com>
2025-06-09 12:20:12 -0400

    fix: fix transitionlink router

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,6 +8,7 @@ import { linkResolver } from "@/lib/linkResolver";
 import { useAtomValue } from "jotai";
 import { pageInfoAtom } from "@/app/atoms/pageInfoAtom";
 import { useTransitionRouter } from "next-view-transitions";
+import { asLink } from "@prismicio/client";
 
 const buttonVariants = cva(
   "cursor-pointer inline-flex items-center justify-center gap-2 rounded-[4px] body-lg transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:outline-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:outline-destructive px-7 py-4 has-[>svg]:px-3 min-w-0 text-center",
@@ -78,16 +79,23 @@ function Button({
 export type TransitionLinkProps = React.ComponentProps<typeof PrismicNextLink>;
 
 const TransitionLink = React.forwardRef<HTMLAnchorElement, TransitionLinkProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, onClick, ...props }, ref) => {
     const pages = useAtomValue(pageInfoAtom);
     const { push } = useTransitionRouter();
 
     const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
-      if (props.href && typeof props.href === "string") {
+      const { field } = props;
+      if (!field) return;
+
+      const resolvedHref = linkResolver
+        ? linkResolver(field as any, pages)
+        : asLink(field); // Fallback to Prismic's default if no resolver provided
+
+      if (resolvedHref) {
         e.preventDefault();
-        push(props.href);
+        push(resolvedHref);
       }
-      props.onClick?.(e);
+      onClick?.(e);
     };
 
     return (
