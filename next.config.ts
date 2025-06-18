937a69db3b263b747408ecc145d3b5a1ae50cc79
zacmelendez <zacmelendez@gmail.com>
2025-06-18 19:49:09 -0400

    feat: added origins for aca

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,16 @@ const nextConfig: NextConfig = {
   // fullUrl: true,
   // },
   // },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "https://aca.org",
+        "https://www.aca.org",
+        "www.aca.org",
+        "aca.org",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
