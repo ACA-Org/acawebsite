a7578b158453757d067892ba061df6ef4596bc11
zacmelendez <zacmelendez@gmail.com>
2025-10-14 14:18:50 -0400

    fix: update image condition to check for url in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -42,11 +42,10 @@ export default async function Page() {
     <div className="flex w-full flex-col">
       <div className="mx-auto mb-12 flex w-full max-w-[1440px] flex-col px-5 md:mb-28 md:px-8">
         <div className="relative mt-16 flex h-full min-h-[300px] w-full shrink-0 items-end gap-2.5 overflow-clip rounded-2xl p-12">
-          {img && (
+          {img.url && (
             <>
               <div className="absolute inset-0 z-10 h-full w-full">
                 <DynamicImage
-                  alt=""
                   field={img}
                   className="h-full w-full object-cover"
                 />
