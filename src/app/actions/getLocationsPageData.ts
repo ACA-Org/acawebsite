fb43556a974080f606b15fc6f976584324a9a20e
zacmelendez <zacmelendez@gmail.com>
2025-12-05 16:29:20 -0500

    fix: added better cache tags

--- src/app/actions/getLocationsPageData.ts
+++ src/app/actions/getLocationsPageData.ts
@@ -4,5 +4,16 @@ import { createClient } from "@/prismicio";
 
 export async function getLocationsPageData() {
   const client = createClient();
-  return (await client.getSingle("locationsPage"))?.data || null;
+  return (
+    (
+      await client.getSingle("locationsPage", {
+        fetchOptions: {
+          next: {
+            tags: ["locationsPage"],
+            revalidate: 60 * 60 * 24 * 30, // Revalidate every 30 days
+          },
+        },
+      })
+    )?.data || null
+  );
 }
